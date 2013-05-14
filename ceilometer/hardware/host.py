ee60ae4fd8f52ef75e0c6945341fd62ec6e74f27
Toni Zehnder <zehndton@students.zhaw.ch>
2013-05-14 11:41:13 +0200

    - completed some comments

--- ceilometer/hardware/host.py
+++ ceilometer/hardware/host.py
@@ -16,7 +16,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-"""DESCRIPTION""" #TODO: DESCRIPTION
+"""Represents a physical device"""
 
 import re
 from subprocess import Popen, PIPE
